1808d6ce0686a8678c451cc49c67ae023cf21a13
J <jbelding16@gmail.com>
2021-12-15 01:12:48 -0500

    more init

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+});
