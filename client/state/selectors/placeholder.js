6c35843fa2d47418bed83194fe66ecfe872ab3d9
J <jbelding16@gmail.com>
2021-12-22 11:24:15 -0500

    Added placeholder recoil selector

---
+++ client/state/selectors/placeholder.js
@@ -0,0 +1,22 @@
+import { selector } from 'recoil';
+import friendsListState from '../atoms/friendsList';
+import friendsListFilterState from '../atoms/friendsListFilter';
+
+const filteredListState = selector({
+  key: 'filteredTodoListState',
+  get: ({ get }) => {
+    const list = get(friendsListState);
+    const filter = get(friendsListFilterState);
+
+    switch (filter) {
+      case 'Show On Errand':
+        return list.filter((item) => item.status);
+      case 'Show Offline':
+        return list.filter((item) => !item.status);
+      default:
+        return list;
+    }
+  },
+});
+
+export default filteredListState;
