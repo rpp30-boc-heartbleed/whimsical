feea3e7b23372f9fd089364f61b0f0594ee8918c
Laweeza <louieshing@gmail.com>
2021-12-21 17:51:20 -0800

    pseudocode for schema refernces

--- server/models/errand.js
+++ server/models/errand.js
@@ -4,9 +4,9 @@ const { Schema } = mongoose;
 
 const errandSchema = new Schema({
   _id: Schema.Types.ObjectId,
-  requestorId: { type: Schema.Types.ObjectId, ref: 'User', default: null }, // Errand requester
-  placeId: { type: Schema.Types.ObjectId, ref: 'Places', default: null }, // Errand location(s)
-  runnerId: { type: Schema.Types.ObjectId, ref: 'User', default: null }, // Errand runner
+  requestorId: { type: Schema.Types.ObjectId, ref: 'User', default: null }, // Errand requester - Should come from User Schema/Profile
+  placeId: { type: Schema.Types.ObjectId, ref: 'Places', default: null }, // Errand location(s) - Should come from User Schema/Map?
+  runnerId: { type: Schema.Types.ObjectId, ref: 'User', default: null }, // Errand runner - - Should come from User Schema/Profile
   errands: { type: Array, default: [] }, // Empty list if no errands
   time: { type: Date, required: true }, // new Date().toLocaleString();
   status: {
