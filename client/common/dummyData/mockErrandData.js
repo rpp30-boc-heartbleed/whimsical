65ba30e79f83c3401c2d5cf1670ec289cc7bae9e
Laweeza <louieshing@gmail.com>
2021-12-27 14:15:23 -0800

    working on mock data

--- client/common/dummyData/mockErrandData.js
+++ client/common/dummyData/mockErrandData.js
@@ -1,26 +1,51 @@
-// const userLocation = {
-//   streetName: '1234 Test Street',
-//   zip: 68576,
-//   location: {
-//     latitude: -12345,
-//     longitude: 36,
-//   },
-// };
+/***
+create new errand with
+1. name of errand
+2. store location
+3. radius
+***/
+
+const testUser = {
+  name: 'requestor',
+  address: {
+    street: '123 Main Street',
+    city: 'Kalamazoo',
+    state: 'MI',
+    zip: '49006',
+  },
+  location: {
+    latitude: 40,
+    longitude: -80,
+  },
+};
 
 const mockErrandsData = [
   {
     id: 1,
     errandName: 'Impromptu Bagel Run',
-    destination: {
-      latitude: -12345,
-      longitutde: 36,
+    addressName: 'Big Apple Bagels',
+    address: {
+      street: '4408 W Main St',
+      city: 'Kalamazoo',
+      state: 'MI',
+      zip: '49006',
     },
+    location: {
+      latitude: 42.2966481,
+      longitude: -85.6436558,
+    },
+    requestor: userLocation,
     errandRunner: {
       avatar: 'https://via.placeholder.com/50',
       name: 'bob',
-      stars: 5, // a number
+      location: {
+        latitude: -12345,
+        longitude: 36,
+      },
+      starRating: 5,
     },
-  }, // end of errand obj
+    status: 'Pending',
+  },
 ];
 
-export default mockErrandsData;
+export default { testUser, mockErrandsData };
