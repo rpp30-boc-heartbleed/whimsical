44d8dce77f4e995a9ad52539bf4fc55878634ee7
Surekha Weinberg <s.kaniget@gmail.com>
2021-12-27 17:20:34 -0800

    create Login form

--- client/components/Login/LoginContainer.jsx
+++ client/components/Login/LoginContainer.jsx
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
+  KeyboardAvoidingView,
   View,
   Text,
   StyleSheet,
   TextInput,
   StatusBar,
   Button,
+  TouchableOpacity,
 } from 'react-native';
 
 const LoginContainer = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>This is the Login Component</Text>
-      <Button
-        title="Go to FriendsList"
-        onPress={() => navigation.push('FriendsList')} // push the name property of the Stack.Screen component as defined in App.jsx
-      />
-      <Button title="Go to Dashboard" onPress={() => navigation.navigate('Dashboard')} />
-      <StatusBar />
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior='padding'>
+      <View>
+        <Text>Welcome to Quick Bagel!</Text>
+        <Button
+          title="Go to FriendsList"
+          onPress={() => navigation.push('FriendsList')} // push the name property of the Stack.Screen component as defined in App.jsx
+        />
+        <Button title="Go to Dashboard" onPress={() => navigation.navigate('Dashboard')} />
+        <StatusBar />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder='email'
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='password'
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={[styles.button, styles.buttonOutline]}
+          onPress={() => {}}
+        >
+          <Text style={styles.buttonOutlineText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.registerContainer}>
+        <Text style={styles.registerText}>Don&apos;t have an acccount?</Text>
+        <Button title='Register' onPress={() => navigation.navigate('Register')} />
+        <Text style={styles.registerText}>now.</Text>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -28,6 +63,52 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputContainer: {
+    width: '80%',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#0782F9',
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    textAlign: 'center',
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: '#0782F9',
+  },
+  registerContainer: {
+    flexDirection: 'row',
+    marginTop: 15,
+  },
+  registerText: {
+    fontSize: 16,
+    marginTop: 9,
   },
 });
 
