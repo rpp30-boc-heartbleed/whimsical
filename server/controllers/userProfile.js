2d6416e40f15a2f7a06d7cca43f9ec92360558c9
Oje Aisiku <oje.aisiku@gmail.com>
2022-01-02 12:44:39 -0500

    profile controller

--- server/controllers/userProfile.js
+++ server/controllers/userProfile.js
@@ -1,13 +1,26 @@
 const { Profile } = require('../models/userProfile');
 
-const placeholder = (req, res) => {
-  Profile.find()
-    .then(() => {
-      res.json({ message: 'this is a placeholder' });
+const get = (req, res) => {
+  console.log('query', req.query);
+  Profile.findOne()
+    .then((data) => {
+      console.log(data);
+      res.json(data);
     })
     .catch((err) => {
       console.log(err);
     });
 };
 
-module.exports = { placeholder };
+const post = (req, res) => {
+  console.log(req.body.formerUser);
+  Profile.findOneAndUpdate(req.body.formerUser, req.body.updatedUser, (err, rslt) => {
+    res.sendStatus(200);
+  });
+};
+
+const image = (req, res) => {
+  res.send('OK');
+};
+
+module.exports = { get, post, image };
