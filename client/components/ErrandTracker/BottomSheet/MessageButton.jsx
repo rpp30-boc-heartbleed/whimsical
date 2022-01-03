a077603e9a54ff90fbf5ff58f08cbdd117c6ff82
Laweeza <louieshing@gmail.com>
2022-01-02 17:31:27 -0800

    made message button smaller and implemented alert for now

--- client/components/ErrandTracker/BottomSheet/MessageButton.jsx
+++ client/components/ErrandTracker/BottomSheet/MessageButton.jsx
@@ -1,13 +1,41 @@
 import * as React from 'react';
-import { Button } from 'react-native-paper';
+import { Button, TouchableOpacity } from 'react-native-paper';
+import { StyleSheet, View, Text, Alert } from 'react-native';
 
 //Click to go eventual message chat room with errand runner
 
-const MessageButton = () => (
-  <Button icon="message" color='#0782F9' compact={true} mode="contained" onPress={() => console.log('Pressed')}>
-    Message
-  </Button>
-);
+const MessageButton = () => {
+  return (
+    <View style={styles.parent}>
+      <Button
+        icon="message"
+        title="Message"
+        color='#0782F9'
+        mode="contained"
+        onPress={() =>
+          Alert.alert('Message Lady Beth')}
+      >
+        <Text style={styles.text}>Message</Text>
+      </Button>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  parent: {
+    width: 120,
+    height: 100,
+    flexDirection: 'row',
+    alignSelf: 'flex-end',
+    paddingHorizontal: 10,
+    marginHorizontal: '10 %',
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 12,
+    fontWeight: 'bold',
+  }
+})
 
 
 
