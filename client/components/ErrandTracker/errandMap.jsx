33c56ab7cfafae782cf97cd569ad17b4d958c2b0
Laweeza <louieshing@gmail.com>
2022-01-04 00:04:59 -0800

    saved

--- client/components/ErrandTracker/errandMap.jsx
+++ client/components/ErrandTracker/errandMap.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 // import { useRecoilState, useRecoilValue } from 'recoil';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker, Polygon } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 // import Geolocation from 'react-native-geolocation-service';
 import * as Location from 'expo-location';
@@ -21,214 +21,138 @@ import {
 } from 'react-native';
 // port errandState from '../../state/atoms/errands'
 import { GOOGLE_MAPS_API_KEY } from '@env';
-// import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions'
-
-
 
 const ErrandMap = () => {
-  const [location, setLocation] = useState(null);
+  const [region, setRegion] = useState({
+    latitude: 42.2966481,
+    longitude: -85.6436558,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const [markers, setMarkers] = useState([42.2966481, -85.6436558]);
+
+  const mapRef = useRef(null);
+
+  let markerCord = {
+    latitude: 42.2966481,
+    longitude: -85.6436558,
+  };
+
+  let destination = {
+    latitude: 42.295906,
+    longitude: -85.601778,
+  };
+
+  //   const onMapPress = e => {
+  //     setMarkers({
+  //       {
+  //         coordinate: e.nativeEvent.coordinate,
+  //         key: id++
+  //       }
+  //     }),
+  // };
+
+  // let urlFetchDistance = https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins='+lat1+','+lng1+'&destinations='+lat2+'%2C'+lng2+'&key=' + "YOUR_GOOGLE_DIRECTIONS_API_KEY";
 
   useEffect(() => {
-    Location.installWebGeolocationPolyfill();
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        console.log(position)
-        const { latitude, longitude } = position.coords;
-        setLocation(
-          {
-            latitude,
-            longitude
-          });
-      },
-      error => {
-        console.log(error.code, error.message)
-      },
-      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    )
-  }, [])
+    if (mapRef.current) {
+      mapRef.current.animateCamera(
+        {
+          center: {
+            latitude: 40.342,
+            longitude: -85.234,
+          },
+          zoom: 15,
+        },
+        5000,
+      );
+    }
+  }, []);
+
+  // const handleRegionChange = region => {
+  //   coordinate = region;
+  //   console.log(region);
+  // };
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle='dark-content' />
-      {location && (
-        <MapView
-          style={styles.map}
-          provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-          showsUserLocation={true}
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={region}
+      >
+        <Marker
+          coordinate={markerCord}
         />
-      )}
+        <Marker
+          coordinate={destination}
+        />
+        <MapViewDirections
+          lineDashPattern={[0]}
+          origin={region}
+          destination={destination}
+          apikey={GOOGLE_MAPS_API_KEY}
+          strokeWidth={7}
+          strokeColor='#669df6'
+        />
+      </MapView>
     </SafeAreaView>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    alignItems: "center",
+    alignItems: 'center',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
-})
+});
 
 export default ErrandMap;
 
-// const ErrandMap = () => {
-//   const [location, setLocation] = useState(null);
+///Geolocation
 
-//   useEffect(() => {
-//     Location.installWebGeolocationPolyfill();
-//     navigator.geolocation.getCurrentPosition(
-//       position => {
-//         console.log(position)
-//         const { latitude, longitude } = position.coords;
-//         setLocation(
-//           {
-//             latitude,
-//             longitude
-//           });
-//       },
-//       error => {
-//         console.log(error.code, error.message)
-//       },
-//       { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-//     )
-//   }, [])
+// useEffect(() => {
+//   Location.installWebGeolocationPolyfill();
+//   navigator.geolocation.getCurrentPosition(
+//     position => {
+//       console.log(position);
+//       const { latitude, longitude } = position.coords;
+//       setLocation(
+//         {
+//           latitude,
+//           longitude,
+//         },
+//       );
+//     },
+//     (error) => {
+//       console.log(error.code, error.message);
+//     },
+//     { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+//   );
+// }, []);
 
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <StatusBar barStyle='dark-content' />
-//       {location && (
-//         <MapView
-//           style={styles.map}
-//           provider={PROVIDER_GOOGLE}
-//           initialRegion={{
-//             latitude: location.latitude,
-//             longitude: location.longitude,
-//             latitudeDelta: 0.0922,
-//             longitudeDelta: 0.0421,
-//           }}
-//           showsUserLocation={true}
-//         />
-//       )}
-//     </SafeAreaView>
-//   )
-// }
-
-
-// const styles = StyleSheet.create({
-//   container: {
-//     ...StyleSheet.absoluteFillObject,
-//     alignItems: "center",
-//   },
-//   map: {
-//     ...StyleSheet.absoluteFillObject,
-//   },
-// })
-
-// export default ErrandMap;
-
-
-// const ErrandMap = () => {
-
-//   const [location, setLocation] = useState([]);
-
-//   useEffect(() => {
-//     navigator.geolocation.getCurrentPosition(
-// sition => {
-//   //         console.log(position);
-//   //         const { latitude, longitude } = position.coords;
-//   //         setLocation({
-//   //           latitude,
-//   //           longitude,
-//   //         });
-// },
-    //       error => {
-//         console.log(error.code, error.message);
-    //       },
-//       {
-//         enableHighAccuracy: true,
-//         timeout: 15000,
-//         maximumAge: 10000,
-//         distanceFilter: 0,
-//       },
-//     );
-
-//   }, []);
-
-//   // useEffect(() => {
-//   //   Geolocation.getCurrentPosition(
-//   //     position => {
-//   //       const { latitude, longitude } = position.coords;
-//   //       setLocation({
-//   //         latitude,
-//   //         longitude,
-//   //       });
-//   //       // console.log(position);
-//   //     },
-//   //     error => {
-//   //       console.log(error.code, error.message);
-//   //     },
-//   //     {
-//   //       enableHighAccuracy: true,
-//   //       timeout: 15000,
-//   //       maximumAge: 10000
-//   //     },
-//   //   );
-
-//   // }, []);
-
-//   // let location = {
-//   //   latitude: 42.2966481,
-//   //   longitude: -85.6436558,
-//   //   latitudeDelta: 0.0922,
-//   //   longitudeDelta: 0.0421,
-//   // }
-
-//   // let destination = {
-//   //   latitude: 42.295906,
-//   //   longitude: -85.601778
-//   // }
-
-//   return (
-//     <>
-//       <View>
-//         {location.latitude}
-//       </View>
+// return (
+//   <SafeAreaView style={styles.container}>
+//     <StatusBar barStyle='dark-content' />
+//     {location && (
 //       <MapView
+//         style={styles.map}
 //         provider={PROVIDER_GOOGLE}
-//         apikey={GOOGLE_MAPS_API_KEY}
-//         region={location}
-//         style={{ height: 325, width: Dimensions.get('window').width }}
-//       >
-
-//         <Marker coordinate={location} title='Marker' />
-//         {/* <Marker coordinate={destination} title='Marker' /> */}
-
-//         <MapViewDirections
-//           lineDashPattern={[0]}
-//           origin={location}
-//           // destination={destination}
-//           apikey={GOOGLE_MAPS_API_KEY}
-//           strokeWidth={7}
-//           strokeColor='#669df6'
-//         />
-
-//       </MapView>
-
-//     </>
-
-//   )
-
-// }
-
-
-
-// export default ErrandMap;
+//         initialRegion={{
+//           latitude: location.latitude,
+//           longitude: location.longitude,
+//           latitudeDelta: 0.0922,
+//           longitudeDelta: 0.0421,
+//         }}
+//         showsUserLocation={true}
+//       />
+//     )}
+//   </SafeAreaView>
+// );
+// };
