8cc28018abe1552b1d4217d36cdc040506b9a855
Laweeza <louieshing@gmail.com>
2022-01-05 11:40:29 -0800

    okay so draggable works but have to long press

--- client/components/ErrandTracker/errandMap.jsx
+++ client/components/ErrandTracker/errandMap.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 // import { useRecoilState, useRecoilValue } from 'recoil';
-import MapView, { PROVIDER_GOOGLE, Marker, Polygon } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 // import Geolocation from 'react-native-geolocation-service';
 import * as Location from 'expo-location';
@@ -23,6 +23,15 @@ import {
 import { GOOGLE_MAPS_API_KEY } from '@env';
 
 const ErrandMap = () => {
+
+  const [coordinates, setCoordinates] = useState([
+    { name: 'Big Apple Bagels', latitide: 42.253502, longitude: -85.5893426 },
+    { name: 'Panda Express', latitide: 42.2334426951981, longitude: -85.58900073414098 },
+    { name: 'D&W Fresh Market', latitude: 42.26027544435399, longitude: -85.61472073607013 },
+    { name: 'YMCA of Greater Kalamazoo', latitide: 42.272923910906655, longitude: -85.59756323495016 },
+    { name: 'FedEx Ground', latitude: 42.26536715984338, longitude: -85.51540124023748 }
+  ])
+
   const [region, setRegion] = useState({
     latitude: 42.2966481,
     longitude: -85.6436558,
@@ -30,9 +39,9 @@ const ErrandMap = () => {
     longitudeDelta: 0.0421,
   });
 
-  const [markers, setMarkers] = useState([42.2966481, -85.6436558]);
+  // const [markers, setMarkers] = useState([42.2966481, -85.6436558]);
 
-  const mapRef = useRef(null);
+  // const mapRef = useRef(null);
 
   let markerCord = {
     latitude: 42.2966481,
@@ -55,20 +64,20 @@ const ErrandMap = () => {
 
   // let urlFetchDistance = https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins='+lat1+','+lng1+'&destinations='+lat2+'%2C'+lng2+'&key=' + "YOUR_GOOGLE_DIRECTIONS_API_KEY";
 
-  useEffect(() => {
-    if (mapRef.current) {
-      mapRef.current.animateCamera(
-        {
-          center: {
-            latitude: 40.342,
-            longitude: -85.234,
-          },
-          zoom: 15,
-        },
-        5000,
-      );
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (mapRef.current) {
+  //     mapRef.current.animateCamera(
+  //       {
+  //         center: {
+  //           latitude: 40.342,
+  //           longitude: -85.234,
+  //         },
+  //         zoom: 15,
+  //       },
+  //       5000,
+  //     );
+  //   }
+  // }, []);
 
   // const handleRegionChange = region => {
   //   coordinate = region;
@@ -79,17 +88,27 @@ const ErrandMap = () => {
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle='dark-content' />
       <MapView
-        ref={mapRef}
         style={styles.map}
         provider={PROVIDER_GOOGLE}
         initialRegion={region}
       >
-        <Marker
-          coordinate={markerCord}
-        />
-        <Marker
-          coordinate={destination}
-        />
+        <View style={styles.container2}>
+          <Marker
+            draggable
+            coordinate={markerCord}
+          >
+            <Callout>
+              <Text>A Bagel Place </Text>
+            </Callout>
+          </Marker>
+          <Marker
+            coordinate={destination}
+          >
+            <Callout>
+              <Text>Some House </Text>
+            </Callout>
+          </Marker>
+        </View>
         <MapViewDirections
           lineDashPattern={[0]}
           origin={region}
@@ -111,6 +130,10 @@ const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
   },
+  // container2: {
+  //   height: 20,
+  //   width: 20,
+  // },
 });
 
 export default ErrandMap;
