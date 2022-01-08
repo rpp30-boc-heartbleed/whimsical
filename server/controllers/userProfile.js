7d24c6b00cd28031fce3b042bb297c02eb4c85e5
Oje Aisiku <oje.aisiku@gmail.com>
2022-01-08 01:00:14 -0500

    pre-feature freeze

--- server/controllers/userProfile.js
+++ server/controllers/userProfile.js
@@ -4,24 +4,16 @@ const express = require('express');
 const multer = require('multer');
 const { createWriteStream } = require('fs');
 
-// By default, the client will authenticate using the service account file
-// specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
-// the project specified by the GOOGLE_CLOUD_PROJECT environment variable. See
-// https://github.com/GoogleCloudPlatform/google-cloud-node/blob/master/docs/authentication.md
-// These environment variables are set automatically on Google App Engine
 const { Storage } = require('@google-cloud/storage');
 const { Errand } = require('../models/errand');
 const { Profile } = require('../models/userProfile');
-// Instantiate a storage client
 const storage = new Storage();
 
 const app = express();
 app.set('view engine', 'pug');
 
-// This middleware is available in Express v4.16.0 onwards
 app.use(express.json());
 
-// A bucket is a container for objects (files).
 const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
 
 const get = (req, res) => {
@@ -73,7 +65,6 @@ const image = (req, res, next) => {
     const publicUrl = `https://storage.googleapis.com/whimsical-quickbagel/${req.files[0].filename}`;
 
     console.log(publicUrl);
-    // res.status(200).send(publicUrl);
     Profile.findOneAndUpdate({ name: req.body.name }, { picture: publicUrl }, (err, rslt) => {
       console.log('new data inserted');
       res.sendStatus(200);
@@ -81,26 +72,6 @@ const image = (req, res, next) => {
   }
 
   uploadFile().catch(console.error);
-
-  // Create a new blob in the bucket and upload the file data.
-  // const blob = bucket.file(req.files[0]);
-  // console.log('blob', blob);
-  // const blobStream = blob.createWriteStream();
-
-  // blobStream.on('error', err => {
-  //   next(err);
-  // });
-
-  // blobStream.on('finish', () => {
-  //   // The public URL can be used to directly access the file via HTTP.
-  //   const publicUrl = format(
-  //     `https://storage.googleapis.com/${bucket.name}/${blob.name}`
-  //   );
-  //   console.log(publicUrl);
-  //   res.status(200).send(publicUrl);
-  // });
-
-  // blobStream.end();
 };
 
 module.exports = { get, post, image };
