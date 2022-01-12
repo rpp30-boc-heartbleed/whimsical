f744c2d6a36ff9ce7c5f5ba180b92f3bf0cda4fc
Laweeza <louieshing@gmail.com>
2022-01-11 22:57:28 -0800

    decided to remove text in navbar

--- client/components/NavBar/NavBarContainer.jsx
+++ client/components/NavBar/NavBarContainer.jsx
@@ -16,36 +16,21 @@ const NavBarContainer = ({ navigation }) => {
       {/* Profile */}
       <TouchableOpacity style={styles.icons}>
         <Icon size={30} color='#3da9fc' name='user' testID='user' onPress={() => navigation.push('UserProfile')} />
-        <Text style={styles.text}>
-          User
-        </Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.icons}>
         <Icon size={30} color='#3da9fc' name='users' testID='users' onPress={() => navigation.push('FriendsList')} />
-        <Text style={styles.text}>
-          Friends
-        </Text>
       </TouchableOpacity>
       {/* Add Errand */}
       <TouchableOpacity style={styles.icons}>
         <Icon size={30} color='#3da9fc' name='plus-circle' testID='plus-circle' onPress={() => navigation.push('NewErrand')} />
-        <Text style={styles.text}>
-          Add Errand
-        </Text>
       </TouchableOpacity>
       {/* FriendsList */}
       <TouchableOpacity style={styles.icons}>
         <Icon size={30} color='#3da9fc' name='shopping-basket' testID='shopping-basket' onPress={() => navigation.push('ErrandRequests')} />
-        <Text style={styles.text}>
-          Errand List
-        </Text>
       </TouchableOpacity>
       {/* Dashboard */}
       <TouchableOpacity style={styles.icons}>
         <Icon size={30} color='#3da9fc' name='home' testID='home' onPress={() => navigation.navigate('Dashboard')} />
-        <Text style={styles.text}>
-          Dashboard
-        </Text>
       </TouchableOpacity>
     </View>
   );
@@ -66,9 +51,6 @@ const styles = StyleSheet.create({
   icons: {
     alignItems: 'center',
     maxWidth: 50,
-  },
-  text: {
-    fontSize: 9.5,
   },
 });
 
